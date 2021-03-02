8b5e377e7b8a8370c0e0130a412c2068b85ded6f
Johan Rönkkö <johan.ronkko1@gmail.com>
2021-03-02 23:24:11 +0100

    Defining routes for quote API.

--- cmd/quote-api/handler/routes.go
+++ cmd/quote-api/handler/routes.go
@@ -4,5 +4,7 @@ import "net/http"
 
 func (s *Handler) routes() {
 	s.router.HandleFunc(http.MethodGet, "/api.v1/healthcheck", s.handleHealthCheck())
+	s.router.HandleFunc(http.MethodGet, "/api.v1/quotes/:id", s.handleGetQuote())
 	s.router.HandleFunc(http.MethodGet, "/api.v1/quotes", s.handleListQuotes())
+	s.router.HandleFunc(http.MethodPost, "/api.v1/quotes", s.handleAddQuote())
 }
